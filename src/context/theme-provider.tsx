b5944d29b679b7a03fcc20d6a6333a159ff3d086
Shajedul Islam Shuvo <shajedul.shuvo@gmail.com>
2025-05-05 01:25:03 +0600

    Updated with localstorage issue

--- src/context/theme-provider.tsx
+++ src/context/theme-provider.tsx
@@ -27,16 +27,16 @@ export function ThemeProvider({
   storageKey = "vite-ui-theme",
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  );
+  // const [theme, setTheme] = useState<Theme>(
+  //   () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  // );
 
-  // const [theme, setTheme] = useState<Theme>(() => {
-  //   if (typeof window !== 'undefined') {
-  //     return (localStorage.getItem(storageKey) as Theme) || defaultTheme;
-  //   }
-  //   return defaultTheme;
-  // });
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (typeof window !== 'undefined') {
+      return (localStorage.getItem(storageKey) as Theme) || defaultTheme;
+    }
+    return defaultTheme;
+  });
 
   useEffect(() => {
     const root = window.document.documentElement;
@@ -66,8 +66,12 @@ export function ThemeProvider({
   const value = {
     theme,
     setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme);
-      setTheme(theme);
+
+      if (typeof window !== 'undefined') {
+        localStorage.setItem(storageKey, theme);
+        setTheme(theme);
+      }
+      
     },
   };
 
